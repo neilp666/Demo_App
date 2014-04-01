5121b8a799b1cca48d30bfdc50a3a88efac33ea8
neil patel <neil@myindianweddingapp.com>
2014-04-01 10:02:34 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'e03fdc6a18be258740ad89d44e39408a99576bee4989ada53698203295489f6ad5d9c17948ce79e7c42ca0db6ca6fddffbe56eda98f4a223bfe0b14805b75fa2'
